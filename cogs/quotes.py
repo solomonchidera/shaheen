09b4d032cc584ae9195b8638fe5c8dbfe90bef8b
Y4h14 <yahyasadig2013@gmail.com>
2024-08-14 16:50:52 +0300

    added documentation

--- cogs/quotes.py
+++ cogs/quotes.py
@@ -12,24 +12,50 @@ class Quotes(commands.Cog):
 
     @app_commands.command(name='quote', description='Get an inspiring quote')
     async def quote_command(self, interaction: discord.interactions):
+        """ Defines "quote" command that gives a qoute to the user.
+
+        Args:
+            interaction (discord.interactions): object for interacting with
+            commands.
+        """
+
         quote: str = get_quote()
         await self.send_quote(interaction, quote)
 
-    async def send_quote(self, interaction, content):
-        """
+    async def send_quote(self, interaction: discord.Interaction, content: str):
+        """ prepare the command response and send it as a response
+            also defines a button and a callback for the command
+        Args:
+            interaction (discord.interaction): command interaction object.
+            content (str): content returned by the command
         """
         new_quote_button = discord.ui.Button(label="New Quote",
                                              style=discord.ButtonStyle.blurple)
 
         async def new_quote_callback(interaction: discord.Interaction):
+            """ Nested recursive function to handle button callback
+
+            Args:
+                interaction (discord.Interaction): command interaction object
+            """
             new_quote = get_quote()
             await self.send_quote(interaction, new_quote)
 
+        # assign the callback function to the button
         new_quote_button.callback = new_quote_callback
+
+        # creating a view and adding the button to the view
         view = discord.ui.View()
         view.add_item(new_quote_button)
+
+        # sending a resonse
         await interaction.response.send_message(content, view=view)
 
 
 async def setup(bot):
+    """ loads the quote Cog into the bot.
+
+    Args:
+        bot (command.Bot): instance of the bot.
+    """
     await bot.add_cog(Quotes(bot))
