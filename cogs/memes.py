8f24c84d0819687844986f3ec4bf07ae168ac2ad
Y4h14 <yahyasadig2013@gmail.com>
2024-08-15 15:29:59 +0300

    Added buttons to meme

--- cogs/memes.py
+++ cogs/memes.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import discord
 from utils.meme_api import get_meme
+import os
 
 
 class Memes(commands.Cog):
@@ -18,8 +19,26 @@ class Memes(commands.Cog):
         """
         """
         meme_link = get_meme()
-        await interaction.response.send_message(meme_link)
+        await self.send_meme(interaction, meme_link)
 
+    async def send_meme(self, interaction: discord.Interaction, content: str) -> None:
+        """
+        """
+        new_meme_button = discord.ui.button(label='New Meme😜',
+                                            style=discord.ButtonStyle.primary)
+        async def new_meme_callback(interaction: discord.Interaction):
+            """_summary_
+            """
+            new_meme = get_meme()
+            await self.send_meme(interaction, new_meme)
+
+        new_meme_button.callback = new_meme_callback
+
+        view = discord.ui.View()
+        view.add_item(new_meme_button)
+        view.add_item(discord.ui.Button(label="Invite", url=os.getenv('INVITE_LINK'), style=discord.ButtonStyle.link))
+
+        await interaction.response.send_message(content, view=view)
 
 async def setup(bot):
     """ loads the memes Cog into the bot.
