3c78280098a4c443f8dae0b1e7199cb809bf13e8
Y4h14 <yahyasadig2013@gmail.com>
2024-08-15 15:34:27 +0300

    Added documentation

--- cogs/memes.py
+++ cogs/memes.py
@@ -7,7 +7,7 @@ import os
 
 
 class Memes(commands.Cog):
-    """
+    """A class fro memes command
     """
     def __init__(self, bot):
         """initalization function"""
@@ -16,18 +16,30 @@ class Memes(commands.Cog):
     @app_commands.command(name='meme',
                           description='A meme to light up your day')
     async def meme_command(self, interaction: discord.Interaction):
-        """
+        """defines and set up the 'meme' command that get's a meme to user.
+ 
+        Args:
+            interaction (discord.Interaction): object for interacting with
+            commands.
         """
         meme_link = get_meme()
         await self.send_meme(interaction, meme_link)
 
     async def send_meme(self, interaction: discord.Interaction, content: str) -> None:
-        """
+        """prepare the command response and send it as a response
+            also defines a button and a callback for the command.
+        Args:
+            interaction (discord.interaction): command interaction object.
+            content (str): content returned by the command.
         """
         new_meme_button = discord.ui.button(label='New Meme😜',
                                             style=discord.ButtonStyle.primary)
         async def new_meme_callback(interaction: discord.Interaction):
-            """_summary_
+            """Nested recursive function to handle button callback.
+               it graps a new meme and send it back to user.
+
+            Args:
+                interaction (discord.Interaction): command interaction object
             """
             new_meme = get_meme()
             await self.send_meme(interaction, new_meme)
