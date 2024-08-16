6cbf20ca8201b5bca0f9240c7378210da3002cde
solomonchidera <solochidera10@gmail.com>
2024-08-16 12:43:12 +0100

    refactor: Add meme functionality to the bot
    This commit adds the necessary code changes to implement the meme functionality in the bot. It includes modifications to the `meme_api.py` file to make a request to the memes API and retrieve a meme image. Additionally, the `memes.py` file in the `cogs` directory is updated to define the `meme` command, which sends a meme to the user in a Discord interaction. This feature enhances the bot's capabilities and provides users with a fun and entertaining experience.

--- cogs/memes.py
+++ cogs/memes.py
@@ -1,1 +1,70 @@
 """defines commands for the memes functionality"""
+from discord.ext import commands
+from discord import app_commands
+import discord
+from utils.meme_api import get_meme
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Memes(commands.Cog):
+    """A class fro memes command
+    """
+    def __init__(self, bot):
+        """initalization function"""
+        self.bot = bot
+
+    @app_commands.command(name='meme',
+                          description='A meme to light up your day')
+    async def meme_command(self, interaction: discord.Interaction):
+        """defines and set up the 'meme' command that get's a meme to user.
+
+        Args:
+            interaction (discord.Interaction): object for interacting with
+            commands.
+        """
+        meme_link = get_meme()
+        await self.send_meme(interaction, meme_link)
+
+    async def send_meme(self,
+                        interaction: discord.Interaction,
+                        content: str) -> None:
+        """prepare the command response and send it as a response
+            also defines a button and a callback for the command.
+        Args:
+            interaction (discord.interaction): command interaction object.
+            content (str): content returned by the command.
+        """
+        new_meme_button = discord.ui.Button(label='New Meme😜',
+                                            style=discord.ButtonStyle.primary)
+        invite_button = discord.ui.Button(label="Invite",
+                                        url=os.getenv('INVITE_LINK'),
+                                        style=discord.ButtonStyle.link)
+
+        async def new_meme_callback(interaction: discord.Interaction):
+            """Nested recursive function to handle button callback.
+               it graps a new meme and send it back to user.
+
+            Args:
+                interaction (discord.Interaction): command interaction object
+            """
+            new_meme = get_meme()
+            await self.send_meme(interaction, new_meme)
+
+        new_meme_button.callback = new_meme_callback
+
+        view = discord.ui.View()
+        view.add_item(new_meme_button)
+        view.add_item(invite_button)
+
+        await interaction.response.send_message(content, view=view)
+
+
+async def setup(bot):
+    """ loads the memes Cog into the bot.
+
+    Args:
+        bot (command.Bot): instance of the bot.
+    """
+    await bot.add_cog(Memes(bot))
--- utils/meme_api.py
+++ utils/meme_api.py
@@ -1,1 +1,30 @@
 """defines a function make a request to the memes API and reatruns a meme"""
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
+
+
+def get_meme():
+    """Make an api request to programming memes rapidapi
+
+    Returns:
+        str: link of a meme
+    """
+
+    url = "https://programming-memes-images.p.rapidapi.com/v1/memes"
+
+    headers = {
+        "x-rapidapi-key": RAPIDAPI_KEY,
+        "x-rapidapi-host": "programming-memes-images.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        image = data[0]['image']
+        return image
+    else:
+        return
