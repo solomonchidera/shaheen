42dd2483ffe03d2cc38925186b26add53e9880bc
solomonchidera <solochidera10@gmail.com>
2024-08-16 07:44:22 +0100

    refactor: Load additional cogs in bot.py
    This commit refactors the `load_cogs` function in `bot.py` to load additional cogs for the bot. The following cogs are now being loaded:
    - `cogs.articles`
    - `cogs.youtube`
    - `cogs.quotes`
    - `cogs.projects`
    - `cogs.about`
    - `cogs.memes`
    
    These cogs provide additional functionality to the bot, enhancing its capabilities. This change was made to improve the overall user experience and provide more features for users to interact with.

--- bot.py
+++ bot.py
@@ -16,13 +16,14 @@ bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 
 # Load cogs
 async def load_cogs():
-     await bot.load_extension('cogs.games')
+    await bot.load_extension('cogs.games')
 #     await bot.load_extension('cogs.challenges')
-     await bot.load_extension('cogs.articles')
-     await bot.load_extension('cogs.youtube')
-     await bot.load_extension('cogs.quotes')
-     await bot.load_extension('cogs.projects')
-#     await bot.load_extension('cogs.memes')
+    await bot.load_extension('cogs.articles')
+    await bot.load_extension('cogs.youtube')
+    await bot.load_extension('cogs.quotes')
+    await bot.load_extension('cogs.projects')
+    await bot.load_extension('cogs.about')
+    await bot.load_extension('cogs.memes')
 
 
 @bot.event
