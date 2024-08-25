3c4c1fa4a7411e2a2c6dd4e6203156f22d19e876
Y4h14 <yahyasadig2013@gmail.com>
2024-08-25 14:54:21 +0300

    updated code styling

--- utils/youtube_api.py
+++ utils/youtube_api.py
@@ -3,8 +3,10 @@ from googleapiclient.discovery import build
 
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 
+
 def get_youtube_service():
     return build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+
 
 def fetch_random_video(topic="technology", max_results=10):
     youtube = get_youtube_service()
